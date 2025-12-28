b7bd724dbcc2be728b20f8151c703a0942b5cc19
Ayush-2511 <ayushbhartii2511@gmail.com>
2025-12-28 21:35:48 +0530

    Used Vector2 from pygame

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import pygame
+from pygame.math import Vector2
 #import random
 
 
-dt = 0.001
+"""dt = 0.001
 x = 100.0
 y = 200.0
 vx = 100.0
 vy = 0.0
 m = 2.0
-g = 9.81 * 200
+g = 9.81 * 200"""
 
+
+
+
+pos = Vector2(100.0, 200.0)
+vel = Vector2(10.0, 0.0)
+m = 2.0
+g = 9.81 * 50
+Force = Vector2(0.0, -m * g)
+acc = Vector2(0.0, 0.0)
 
 pygame.init()
 width,height = 500,500
@@ -25,37 +35,32 @@ ground = height - 50
 
 running = True
 while running:
-    clock.tick(600)
+    dt = clock.tick(60) / 1000
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    #X- corrdinate
-    Fx = 0.0
-    ax = Fx/m
-    vx = vx + ax*dt
-    x = x + vx*dt
 
-    #Y- corrdinate
-    Fy = -m *g
-    ay = Fy/m
-    vy = vy + ay*dt
-    y = y + vy*dt
+
+    Force = Vector2(0.0, -m * g)
+    acc = Force / m
+    vel += acc * dt
+    pos += vel * dt
+
 
     r = 16
     e = 0.9
-    if y <= r:
-        y = r
-        vy = -e * vy       
+    if pos.y <= r:
+        pos.y = r
+        vel.y = -e * vel.y       
 
 
     screen.fill((20,20,20))
 
     pygame.draw.line(screen,(200,200,200),(0,ground), (width,ground),5)
 
-    screeny = ground - y
-    pygame.draw.circle(screen, (0, 255, 0),(x, int(screeny)),r)
+    screeny = ground - pos.y
+    pygame.draw.circle(screen, (0, 255, 0),(int(pos.x), int(screeny)),r)
 
     pygame.display.flip()
 
