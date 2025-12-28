5cbaf801a8d94d7a347d65e75becd58f2c22b574
Ayush-2511 <ayushbhartii2511@gmail.com>
2025-12-28 22:01:49 +0530

    changed to numpy at place of Vector2

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 import pygame
-from pygame.math import Vector2
+import numpy as np
 #import random
 
 
-"""dt = 0.001
-x = 100.0
-y = 200.0
-vx = 100.0
-vy = 0.0
-m = 2.0
-g = 9.81 * 200"""
 
-
-
-
-pos = Vector2(100.0, 200.0)
-vel = Vector2(10.0, 0.0)
+pos = np.array([100.0, 200.0])
+vel = np.array([10.0, 0.0])
 m = 2.0
 g = 9.81 * 50
-Force = Vector2(0.0, -m * g)
-acc = Vector2(0.0, 0.0)
+Force = np.array([0.0, -m * g])
+acc = np.array([0.0, 0.0])
 
 pygame.init()
 width,height = 500,500
@@ -42,7 +32,7 @@ while running:
             running = False
 
 
-    Force = Vector2(0.0, -m * g)
+    Force = np.array([0.0, -m * g])
     acc = Force / m
     vel += acc * dt
     pos += vel * dt
@@ -50,17 +40,17 @@ while running:
 
     r = 16
     e = 0.9
-    if pos.y <= r:
-        pos.y = r
-        vel.y = -e * vel.y       
+    if pos[1] <= r:
+        pos[1] = r
+        vel[1] = -e * vel[1]       
 
 
     screen.fill((20,20,20))
 
     pygame.draw.line(screen,(200,200,200),(0,ground), (width,ground),5)
 
-    screeny = ground - pos.y
-    pygame.draw.circle(screen, (0, 255, 0),(int(pos.x), int(screeny)),r)
+    screeny = ground - pos[1]
+    pygame.draw.circle(screen, (0, 255, 0),(int(pos[0]), int(screeny)),r)
 
     pygame.display.flip()
 
